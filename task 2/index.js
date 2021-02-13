e5378e42df0f464a10a1746b899d5668fd15a495
apekhnik <iam3ji@gmail.com>
2021-02-13 12:46:40 +0300

    task2

--- task 2/index.js
+++ task 2/index.js
@@ -12,7 +12,6 @@ const server = http.createServer((req, res) => {
 
 var { data, condition } = JSON.parse(fs.readFileSync("./data.json", "utf8"));
 function sort(data) {
-  // const copy = JSON.parse(JSON.stringify(data));
   let filtered = data
     .sort((a, b) => b.rating - a.rating)
     .filter((item, i, arr) => (item.disabled != false ? 0 : 1));
@@ -20,11 +19,7 @@ function sort(data) {
   return filtered;
 }
 console.log(sort(data));
-// console.log(data, condition);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-// let arr = [2, 3, 4, 5, 6, 7, 8];
-// let n = arr.filter((item, i, arr) => {});
-// console.log(n);
