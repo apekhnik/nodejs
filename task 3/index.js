b97e073afeef05c90305447d48e1e6d1dc8484fc
apekhnik <iam3ji@gmail.com>
2021-02-14 14:44:24 +0300

    task3

--- task 3/index.js
+++ task 3/index.js
@@ -16,27 +16,21 @@ function findSplitPoint(arr, n) {
   leftSum = 0;
   let A = [];
   let B = set;
-  // traverse array element
-  for (let i = 0; i < n; i++) {
-    // add current element
-    // to left Sum
-    leftSum += arr[i];
-    console.log("left sum:", leftSum);
-    // find sum of rest array
-    // elements (rightSum)
-    let rightSum = 0;
-    for (j = i + 1; j < n; j++) rightSum += arr[j];
 
-    // split point index
+  for (let i = 0; i < n; i++) leftSum += arr[i];
+
+  let rightSum = 0;
+  for (i = n - 1; i >= 0; i--) {
+    rightSum += arr[i];
+    leftSum -= arr[i];
+
     if (leftSum == rightSum) {
-      A = B.splice(3);
+      console.log(A);
+      A = B.splice(i);
       return { A, B };
     }
   }
 
-  // if it is not possible
-  // to split array into
-  // two parts
   return -1;
 }
 console.log(findSplitPoint(set, set.length));
