a0ba847fe20c3a610f13fb460662be63a51ced6d
apekhnik <iam3ji@gmail.com>
2021-02-15 20:28:52 +0300

    task 3

--- task 3/index.js
+++ task 3/index.js
@@ -9,23 +9,44 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
+const getLocalFile = (path) => JSON.parse(fs.readFileSync(path, "utf8"));
 
-var { set } = JSON.parse(fs.readFileSync("./data.json", "utf8"));
+const { set } = getLocalFile("./data.json");
 
 function findSplitPoint(arr, n) {
   leftSum = 0;
-  let A = [];
-  let B = set;
-
+  let A = set;
+  let B = [];
+  //заполняю leftsum
   for (let i = 0; i < n; i++) leftSum += arr[i];
 
   let rightSum = 0;
+  //прохожу циклом с конца массива, поочередно плюсуя елемент в правую сумму
+  // вычитая из левой.
   for (i = n - 1; i >= 0; i--) {
     rightSum += arr[i];
     leftSum -= arr[i];
-
+    //если суммы равны, делю массив по индексу i и возвращаю результат
     if (leftSum == rightSum) {
-      A = B.splice(i);
+      B = A.splice(i);
+      return { A, B };
+    }
+    //если произошел перебор, и правая суммая стала больше, вычисляем на сколько больше.
+    if (leftSum < rightSum) {
+      B = A.splice(i);
+      const difference = rightSum - leftSum;
+
+      // Ищем в большем массиве половину разности
+      // если она есть, переносим в другой массив и они равны, если нет, ищем приближенно меньшую и переносим ее
+      if (B.indexOf(difference / 2) === -1) {
+        for (let i = 0; i < B.length; i++) {
+          difference / 2 > B[i] ? A.push(B.splice(i, 1)[0]) : null;
+        }
+      } else {
+        let n = B.indexOf(difference / 2);
+        A.push(B.splice(n, 1)[0]);
+      }
+
       return { A, B };
     }
   }
